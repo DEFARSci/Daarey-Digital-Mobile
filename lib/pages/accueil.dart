bb90e09e5a3945a9eb6fd854640909102c74ef5f
presi7 <ndoyeteranga777@gmail.com>
2025-01-14 12:47:50 +0000

    ajout dossier assets

--- lib/pages/accueil.dart
+++ lib/pages/accueil.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 class Accueil extends StatefulWidget {
   const Accueil({super.key});
@@ -8,8 +9,151 @@ class Accueil extends StatefulWidget {
 }
 
 class _AccueilState extends State<Accueil> {
+  late VideoPlayerController _videoController;
+
+  @override
+  void initState() {
+    super.initState();
+    // Initialisation du lecteur vidéo local
+    _videoController = VideoPlayerController.asset(
+      'assets/videos/Diangue Du Wéss dessin animé pour les enfant leçon 01 surat fatiha.mp4', // Chemin de la vidéo locale
+    )..initialize().then((_) {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    _videoController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Accueil"),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Section Vidéo avec contrôles
+            Container(
+              height: 200,
+              width: double.infinity,
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  // Vidéo
+                  _videoController.value.isInitialized
+                      ? AspectRatio(
+                    aspectRatio: _videoController.value.aspectRatio,
+                    child: VideoPlayer(_videoController),
+                  )
+                      : const Center(child: CircularProgressIndicator()),
+                  // Bouton Lecture/Pause
+                  IconButton(
+                    iconSize: 50,
+                    icon: Icon(
+                      _videoController.value.isPlaying
+                          ? Icons.pause_circle_filled
+                          : Icons.play_circle_fill,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        if (_videoController.value.isPlaying) {
+                          _videoController.pause();
+                        } else {
+                          _videoController.play();
+                        }
+                      });
+                    },
+                  ),
+                ],
+              ),
+            ),
+
+            // Section Titre et Description
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Nos actualités",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+                        "Praesent vitae eros eget tellus tristique bibendum.",
+                    style: TextStyle(fontSize: 16, color: Colors.grey),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+
+      // Barre de Navigation
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Accueil",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            label: "Cours",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.library_books),
+            label: "Hadith et Dogme",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.help),
+            label: "FAQ",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.lock),
+            label: "Salon privé",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: "Faire un don",
+          ),
+        ],
+        currentIndex: 0,
+        onTap: (index) {
+          switch (index) {
+            case 0:
+              break;
+            case 1:
+              Navigator.pushNamed(context, '/cours');
+              break;
+            case 2:
+              Navigator.pushNamed(context, '/hadith');
+              break;
+            case 3:
+              Navigator.pushNamed(context, '/faq');
+              break;
+            case 4:
+              Navigator.pushNamed(context, '/salon');
+              break;
+            case 5:
+              Navigator.pushNamed(context, '/don');
+              break;
+          }
+        },
+      ),
+    );
   }
 }
